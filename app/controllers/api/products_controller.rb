12d15f11f4853a5caf85074069a3a8a4438d486e
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-12 21:09:16 -0500

    routes to display JSON for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @all_cameras = Product.all
     render 'camera_store.json.jb'
   end
+
+  def camera_brand_sony
+    @sony_camera = Product.find_by(id: 1)
+    render 'sony.json.jb'
+  end
+
+  def camera_brand_canon
+    @canon_camera = Product.find_by(id: 2)
+    render 'canon.json.jb'
+  end
+
+  def camera_brand_nikon
+    @nikon_camera = Product.find_by(id: 3)
+    render 'nikon.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/camera_shop" => "products#camera_store"
+    get "/camera_sony" => "products#camera_brand_sony"
+    get "/camera_canon" => "products#camera_brand_canon"
+    get "/camera_nikon" => "products#camera_brand_nikon"
   end
 end
