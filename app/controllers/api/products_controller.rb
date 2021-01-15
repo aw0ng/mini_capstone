51df26305574045e26436285fbf29950ba36e3f9
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-14 21:29:14 -0500

    update with all restful conventions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,4 +8,31 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render 'show.json.jb'
   end
+
+  def create
+    @product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    @product.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    @product.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id]) #do not need instance, not viewing
+    product.destroy #do not need @ to create instance, not viewing
+    render json: {message: 'product destroyed'}
+  end
 end
