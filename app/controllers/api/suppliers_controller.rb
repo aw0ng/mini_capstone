eb3da0503f4ce31b1b7c226c237e910c62c2ff63
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-20 22:29:10 -0500

    created suppliers CRUD

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -24,6 +24,7 @@ class Api::SuppliersController < ApplicationController
 
   def update
     @supplier = Supplier.find_by(id: params[:id])
+    @supplier.name = params["name"] || @supplier.name
     @supplier.email = params["email"] || @supplier.email
     @supplier.phone_number = params["phone_number"] || @supplier.phone_number
     @supplier.save
