1c288d40f4c0b06050e8e9fafa02f1944605b98e
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-26 22:22:53 -0500

    new category_product join database with many-to-many relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :category_products
   validates :name, presence: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0}
