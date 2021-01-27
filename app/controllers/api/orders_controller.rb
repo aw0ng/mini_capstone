8d310f89a82d392cb65b307bdfc12ccaade74d80
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-26 19:28:19 -0500

    add application_controller authenticate_user method to orders_controller instead of if conditions

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,49 +1,39 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+  
   def index
     # @orders = Order.where(user_id: current_user.id)
-    if current_user
-      @orders = current_user.orders
-      render "index.json.jb"
-    else
-      render json: [], status: :unauthorized
-    end
+    @orders = current_user.orders
+    render "index.json.jb"
   end
   
   def show
-    if current_user
-      @order = current_user.orders.find_by(id: params[:id])
-      render "show.json.jb"
+    @order = current_user.orders.find_by(id: params[:id])
+    render "show.json.jb"
     # @order = Order.find_by(id: params[:id])
     # if @order.user_id == current_user.id
     #   render "show.json.jb"
     # else
     #   render json: { message: "You must be logged in to see your orders."}
     # end
-    else
-      render json: [], status: :unauthorized
-    end
   end
   
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
-      calculated_subtotal = product.price * params[:quantity].to_i
-      calculated_tax = calculated_subtotal * 0.09
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
     # create tax variable to store tax rate separately
-      calculated_total = calculated_subtotal + calculated_tax
+    calculated_total = calculated_subtotal + calculated_tax
 
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total: calculated_total
-        )
-      @order.save
-      render "show.json.jb"
-    else
-      render json: { errors: @order.errors.full_messages }, status: 422
-    end
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+      )
+    @order.save
+    render "show.json.jb"
   end
 end
